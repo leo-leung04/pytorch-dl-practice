f389fd3cf58b2622e5e5c3561ce1958c243d9f4e
Jingcheng Liang <jcliang316@gmail.com>
2025-01-23 16:52:06 -0600

    Complete the training loop, testing loop and main function for GNN1-GCN

--- GNN/GNN1-GCN.py
+++ GNN/GNN1-GCN.py
@@ -113,3 +113,79 @@ if 'IS_GRADESCOPE_ENV' not in os.environ:
               x = F.dropout(x, p=self.dropout, training=self.traning)
         ##########################################
         return out
+
+  #############
+  # Training loop and Testing loop
+  #############
+
+def train(model, data, train_idx, optimizer, loss_fn):
+    model.train()
+    optimizer.zero_grad()
+    out = model(data.x, data.adj_t)
+    loss = loss_fn(out[train_idx], data.y[train_idx].squeeze(1))
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+  
+@torch.no_grad()
+def test(model, data, split_idx, evaluator):
+    model.eval()
+    out = model(data.x, data.adj_t)
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    train_acc = evaluator.eval({
+        'y_true': data.y[split_idx['train']],
+        'y_pred': y_pred[split_idx['train']],
+    })['acc']
+    valid_acc = evaluator.eval({
+        'y_true': data.y[split_idx['valid']],
+        'y_pred': y_pred[split_idx['valid']],
+    })['acc']
+    test_acc = evaluator.eval({
+        'y_true': data.y[split_idx['test']],
+        'y_pred': y_pred[split_idx['test']],
+    })['acc']
+    return train_acc, valid_acc, test_acc
+  
+args = {
+    'device': device,
+    'num_layers': 3,
+    'hidden_dim': 256,
+    'dropout': 0.5,
+    'lr': 0.01,
+    'epochs': 100,
+}
+
+
+
+#############
+# Main function
+#############
+model = GCN(data.num_features, args['hidden_dim'],
+            dataset.num_classes, args['num_layers'],
+            args['dropout']).to(device)
+evaluator = Evaluator(name='ogbn-arxiv')
+
+import copy
+
+# reset the parameters to initial random value
+model.reset_parameters()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
+loss_fn = F.nll_loss
+
+best_model = None
+best_valid_acc = 0
+
+for epoch in range(1, 1 + args["epochs"]):
+    loss = train(model, data, train_idx, optimizer, loss_fn)
+    result = test(model, data, split_idx, evaluator)
+    train_acc, valid_acc, test_acc = result
+    if valid_acc > best_valid_acc:
+        best_valid_acc = valid_acc
+        best_model = copy.deepcopy(model)
+    print(f'Epoch: {epoch:02d}, '
+        f'Loss: {loss:.4f}, '
+        f'Train: {100 * train_acc:.2f}%, '
+        f'Valid: {100 * valid_acc:.2f}% '
+        f'Test: {100 * test_acc:.2f}%')
